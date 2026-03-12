494665d592deea3ed1a9570eac203580bb5495b9
Jade Hernandez <jade.hernandez88@gmail.com>
2026-03-12 14:04:27 +0100

    Header + Navbar implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Navbar from "./components/navigation/navbar";
+
 function App() {
   return (
-    <main className='mx-auto flex min-h-screen w-full min-w-dvw flex-col items-center'>
-      {/* Here goes the component... */}
-    </main>
+    <div className='mx-auto flex min-h-screen w-full min-w-dvw flex-col items-center bg-linear-to-r from-[#F9FAFB] to-[#D2D6DB] p-4'>
+      <Navbar />
+      <main className=''></main>
+    </div>
   );
 }
 
